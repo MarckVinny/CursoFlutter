40d5111d66eb48528885798810ab48bbad1e3e96
MarckVinny <mvrpj@msn.com>
2021-11-24 23:02:38 -0300

    Criação do Componente Resultado.

---
+++ lib/resultado.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class Resultado extends StatelessWidget {
+  final String texto;
+
+  Resultado(this.texto);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+        child: Text(
+      texto,
+      style: const TextStyle(fontSize: 28),
+    ));
+  }
+}
