ba5e0e482ea4d26893fca47ae8d1e27834c46174
MarckVinny <mvrpj@msn.com>
2021-11-25 01:34:57 -0300

    .

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,8 @@
 // ignore_for_file: use_key_in_widget_constructors
 
 import 'package:flutter/material.dart';
-import 'questao.dart';
-import 'resposta.dart';
-import 'resultado.dart';
+import './questionario.dart';
+import './resultado.dart';
 
 main() => runApp(PerguntaApp());
 
@@ -46,14 +45,6 @@ class _PerguntaAppState extends State<PerguntaApp> {
 
   @override
   Widget build(BuildContext context) {
-    // Cria uma Lista contendo as respostas da Pergunta
-    // Se respostas tiver Pergunta Selecionada,
-    // mostra a Lista de Respostas
-    // caso contrário Não mostra nada.
-    List<String> respostas = temPerguntaSelecionada
-        ? _perguntas[_perguntaSelecionada].cast()['respostas']
-        : [];
-
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
@@ -62,30 +53,11 @@ class _PerguntaAppState extends State<PerguntaApp> {
         // Se body tiver uma Pergunta Selecionada
         // Mostra a Column, caso contrário Não mostra nada.
         body: temPerguntaSelecionada
-            ? Column(children: [
-                Questao(_perguntas[_perguntaSelecionada]['texto'].toString()),
-                // Pegar as Respostas, converter a Lista de Strings
-                // em Lista de Widgets e transformar o resultado do Map
-                // em uma Lista e usar o Spread "..." pra colocar a
-                // Lista dentro dos Filhos de Column.
-                ...respostas.map((t) => Resposta(t, _responder)).toList(),
-/*           ElevatedButton(
-            onPressed: () {
-              // ignore: avoid_print
-              print('Resposta 2 foi selecionada');
-            },
-            child: const Text('Resposta 2'),
-          ),
-          ElevatedButton(
-            // ignore: avoid_print
-            onPressed: () => print('Resposta 3!!!'),
-            child: const Text('Resposta 3'),
-          ),
-          ElevatedButton(
-            onPressed: () {},
-            child: const Text('Resposta 4'),
-          ), */
-              ])
+            ? Questionario(
+                perguntas: _perguntas,
+                perguntaSelecionada: _perguntaSelecionada,
+                quandoResponder: _responder,
+              )
             : Resultado('Parabéns!!!'),
       ),
     );
