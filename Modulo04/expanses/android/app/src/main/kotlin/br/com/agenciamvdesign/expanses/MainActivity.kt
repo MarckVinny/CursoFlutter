49e463ac55c50cccd3a84a3ab3d56dd56c05e93b
MarckVinny <mvrpj@msn.com>
2021-12-12 01:34:02 -0300

    novo projeto

--- Modulo04/expanses/android/app/src/main/kotlin/br/com/agenciamvdesign/expanses/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package br.com.agenciamvdesign.expanses
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
---
+++ Modulo04/expenses/android/app/src/main/kotlin/br/com/agenciamvdesign/expenses/MainActivity.kt
@@ -0,0 +1,6 @@
+package br.com.agenciamvdesign.expenses
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
