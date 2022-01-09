f1679a3f509cb79c543b14948199349bfa790ffd
MarckVinny <mvrpj@msn.com>
2022-01-09 19:47:08 -0300

    Aula 105 - Criando o Componente TransctionList()

---
+++ Modulo04/expenses/lib/components/transaction_list.dart
@@ -0,0 +1,62 @@
+import 'package:expenses/models/transaction.dart';
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import '../models/transaction.dart';
+
+class TransactionList extends StatelessWidget {
+  final List<Transaction> transactions;
+
+  // ignore: use_key_in_widget_constructors
+  const TransactionList(this.transactions);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: transactions.map((tr) {
+        return Card(
+          child: Row(
+            children: [
+              Container(
+                margin: const EdgeInsets.symmetric(
+                  horizontal: 15,
+                  vertical: 10,
+                ),
+                decoration: BoxDecoration(
+                    border: Border.all(
+                  color: Colors.purple,
+                  width: 2,
+                )),
+                padding: const EdgeInsets.all(10),
+                child: Text(
+                  'R\$ ${tr.value.toStringAsFixed(2)}',
+                  style: const TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20,
+                    color: Colors.purple,
+                  ),
+                ),
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(tr.title,
+                      style: const TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 16,
+                        color: Colors.black,
+                      )),
+                  Text(
+                    DateFormat('dd-MM-y').format(tr.date),
+                    style: TextStyle(
+                      color: Colors.grey[600],
+                    ),
+                  ),
+                ],
+              )
+            ],
+          ),
+        );
+      }).toList(),
+    );
+  }
+}
