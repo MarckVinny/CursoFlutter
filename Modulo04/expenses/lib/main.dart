42a1616f008eef33319f0baef2920b648c74205d
MarckVinny <mvrpj@msn.com>
2022-01-27 03:15:10 -0300

    .

--- Modulo04/expenses/lib/main.dart
+++ Modulo04/expenses/lib/main.dart
@@ -36,7 +36,7 @@ class ExpensesApp extends StatelessWidget {
                 .copyWith(
                     headline6: const TextStyle(
                         fontFamily: 'OpenSans',
-                        fontSize: 2,
+                        fontSize: 20,
                         fontWeight: FontWeight.bold))
                 .headline6),
       ),
