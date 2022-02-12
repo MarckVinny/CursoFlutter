6476ffbcde16aa489e81c2582640f1aba212ba2e
MarckVinny <mvrpj@msn.com>
2022-02-12 03:04:43 -0300

    Função que irá a Lista de Transações do dia da semana.

--- Modulo04/expenses/lib/components/chart.dart
+++ Modulo04/expenses/lib/components/chart.dart
@@ -3,8 +3,22 @@ import 'package:flutter/material.dart';
 class Chart extends StatelessWidget {
   const Chart({Key? key}) : super(key: key);
 
+  List<Map<String, Object>> get groupedTransaction {
+    return List.generate(7, (index) {
+      return {'day': 'T', 'value': 9.99};
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
+    /** 
+       ** !  elevation: 6, -> cria um efeito de sombra no Card;
+       ** margin: EdgesInserts.all(20), -> cria uma margem para que os componentes 
+       ** não fiquem grudados;
+       ** child: Row(), -> cria uma linha onde serão colocados os Componentes;
+       ** children: [], -> cria uma Lista onde serão colocados os Componentes 
+       ** propriamente dito;
+       **/
     return Card(
       elevation: 6,
       margin: const EdgeInsets.all(20),
