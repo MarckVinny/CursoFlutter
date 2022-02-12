645c73fef434ab199d90ae5158e08ba8c17b2fb0
MarckVinny <mvrpj@msn.com>
2022-02-12 01:34:03 -0300

    Aula 121 - Criando o Componente Chart

--- Modulo04/expenses/lib/components/chart.dart
+++ Modulo04/expenses/lib/components/chart.dart
@@ -5,14 +5,6 @@ class Chart extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    /** 
-       ** !  elevation: 6, -> cria um efeito de sombra no Card;
-       ** margin: EdgesInserts.all(20), -> cria uma margem para que os componentes 
-       ** não fiquem grudados;
-       ** child: Row(), -> cria uma linha onde serão colocados os Componentes;
-       ** children: [], -> cria uma Lista onde serão colocados os Componentes 
-       ** propriamente dito;
-       **/
     return Card(
       elevation: 6,
       margin: const EdgeInsets.all(20),
